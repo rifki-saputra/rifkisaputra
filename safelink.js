999df9c76d4e3548bc8d361d73009642e16ece12
rifki-saputra <74690994+rifki-saputra@users.noreply.github.com>
2020-11-26 12:16:27 +0700

    rifkisaputrajs
    sad

---
+++ safelink.js
@@ -0,0 +1,53 @@
+rifkiclash234@gmail.com
+key:hPF7tFayrTxDNIGDxKfcpw
+
+
+https://drive.google.com/file/d/1084AY2i4722h5g1098AW4TtLzBKxkO7N/preview
+https://drive.google.com/file/d/1XM2hRJEAhQ5HNKkOi9O4CtIs0cRlFG0g/preview
+http://filmgratisanku.my.id/downloadtitle/
+<script type='text/javascript'>
+//<![CDATA[
+var blog = document.location.hostname;
+var slug = document.location.pathname;
+var ctld = blog.substr(blog.lastIndexOf("."));
+var currentURL=location.href;
+var str = currentURL;
+  var res = str.replace("http://website-anda.com/safelink.html?url=", "");
+$(".Visit_Link").hide();
+function changeLink(){
+    var decodedString = Base64.decode(res);
+    window.open(decodedString,'_self')
+}
+
+function generate() {
+    var linkDL = document.getElementById("download"),
+        linkAL = document.getElementById("download2"),
+        btn = document.getElementById("btn"),
+        notif = document.getElementById("daplong"),
+        direklink = document.getElementById("download").href,
+        waktu = 4;
+    var teks_waktu = document.createElement("span");
+    linkDL.parentNode.replaceChild(teks_waktu, linkDL);
+    var id;
+    id = setInterval(function () {
+        waktu--;
+        if (waktu < 0) {
+            teks_waktu.parentNode.replaceChild(linkDL, teks_waktu);
+            clearInterval(id);
+            notif.style.display = "none";
+            linkDL.style.display = "inline";
+            linkAL.style.display = "inline";
+        } else {
+            teks_waktu.innerHTML = "<h3 class='text-danger'>Link will appear in " + waktu.toString() + " Second</h3>";
+            btn.style.display = "none";
+        }
+    }, 2000);
+}
+
+cookieOptions = {
+    msg: "This site uses cookies to help deliver services. By using this site, you agree to the use of cookies.",
+    link: "https://www.blogger.com/go/blogspot-cookies",
+    close: "Got it!",
+    learn: "Learn More" };
+//]]>
+</script>
